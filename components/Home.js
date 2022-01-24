12b8a9cf0e5dd207144b80842d88cd050e269b75
galthop <hitenpatel365@gmail.com>
2022-01-24 11:48:28 +0000

    navigation to fridgelist

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,10 +12,18 @@ const styles = StyleSheet.create({
   },
 });
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Home page</Text>
+      <Text>Next page button below</Text>
+      <Button
+        title='Next page'
+        onPress={() =>
+          navigation.navigate('FridgeList', {
+            test: 'Info with a space',
+          })
+        }
+      />
     </View>
   );
 };
