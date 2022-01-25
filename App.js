6ccd80d4c90f930ee5d187a3c1fdd3d1751297e7
galthop <hitenpatel365@gmail.com>
2022-01-25 12:33:42 +0000

    navbar created

--- App.js
+++ App.js
@@ -1,37 +1,77 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-// import { Text, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import Home from './components/Home';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Ionicons,
+} from '@expo/vector-icons';
 import FridgeList from './components/FridgeList';
 import AddItem from './components/AddItem';
-import Item from './components/Item';
-import TestComponent from './components/TestComponent';
+import Home from './components/Home';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="FridgeList"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="FridgeList"
+        component={FridgeList}
+        options={{
+          tabBarLabel: 'Your fridge',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="fridge" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AddItem"
+        component={AddItem}
+        options={{
+          tabBarLabel: 'Add Item',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="add-circle" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Camera"
+        component={Home}
+        options={{
+          tabBarLabel: 'Camera',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="camera" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Recipes"
+        component={Home}
+        options={{
+          tabBarLabel: 'Recipes',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="reader" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Item" component={Item} />
-        <Stack.Screen
-          name="FridgeList"
-          component={FridgeList}
-          options={{ title: 'Your fridge' }}
-        />
-        <Stack.Screen name="AddItem" component={AddItem} />
-      </Stack.Navigator>
-      <TestComponent />
+      <MyTabs />
     </NavigationContainer>
   );
 }
