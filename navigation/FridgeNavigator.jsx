9cadc8a1ad014b69545b2c09451dfce611c60c14
galthop <hitenpatel365@gmail.com>
2022-01-26 10:25:44 +0000

    native-stack used over stack to remove warning

--- navigation/FridgeNavigator.jsx
+++ navigation/FridgeNavigator.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FridgeList from '../components/FridgeList';
 import Item from '../components/Item';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 const FridgeNavigator = () => {
   return (
     <Stack.Navigator>
