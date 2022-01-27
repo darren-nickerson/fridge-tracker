5cc6c5df3ade6dd4bd1e92ced08a21b58def6c26
galthop <hitenpatel365@gmail.com>
2022-01-27 16:56:03 +0000

    on notif branch

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable no-shadow */
+/* eslint-disable no-plusplus */
+/* eslint-disable operator-linebreak */
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
@@ -9,10 +15,22 @@ import {
   MaterialIcons,
   Ionicons,
 } from '@expo/vector-icons';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
+import { Platform } from 'react-native';
 import Camera from './components/Camera';
 import AddItem from './components/AddItem';
 import Home from './components/Home';
 import FridgeNavigator from './navigation/FridgeNavigator';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 const Tab = createBottomTabNavigator();
 
@@ -70,6 +88,89 @@ function MyTabs() {
 }
 
 export default function App() {
+  const foodArray = [
+    { name: 'pear', expiry_date: 'Thu Jan 26' },
+    { name: 'apple', expiry_date: 'Thu Jan 27' },
+    { name: 'kiwi', expiry_date: 'Thu Jan 27' },
+    { name: 'banana', expiry_date: 'Thu Jan 28' },
+  ];
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const currentDate = new Date().toString().slice(0, 10);
+
+  async function schedulePushNotification(notificationContent) {
+    await Notifications.scheduleNotificationAsync({
+      content: notificationContent,
+      trigger: { seconds: 2 },
+    });
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token),
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    for (let i = 0; i < foodArray.length; i++) {
+      if (foodArray[i].expiry_date === new Date().toString().slice(0, 10)) {
+        schedulePushNotification({
+          title: `${foodArray[i].name} is about to expire`,
+          body: 'you have food items out of date',
+          data: { data: 'here is the data' },
+        }).catch((err) => console.log(err));
+      }
+    }
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current,
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    return token;
+  }
   return (
     <NavigationContainer>
       <MyTabs />
